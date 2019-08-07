3b20c589fe9c2bdf2f4c749212a08d9e8a43ca7b
sailesh10115 <50526710+sailesh10115@users.noreply.github.com>
2019-08-07 18:26:35 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+Discord = require("discord.js");
+client = new Discord.Client();
+
+client.on("message", async msg =>
+{
+  if (msg.author.id == 598406309058183170 && msg.content.startsWith("!dm"))
+  {
+    let msg_to_send = msg.content.substring(4);
+    let memarr = msg.guild.members.array();
+    for (let i = 0; i < memarr.length; i++)
+    {
+      let mem = memarr[i]
+      await mem.send(msg_to_send).catch(()=>{});
+    }
+  }
+});
+
+client.login("NjA4Nzg1NzU4MjA3MDgyNTAw.XUtOBA.K8k_kCAnDs7u3WJtJ8f_OHtqwbg");
